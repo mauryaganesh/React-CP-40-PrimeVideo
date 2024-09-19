21512e6ae43be8bd71d845fd8a272893b57cffaf
Ganesh Maurya <starganesh.maurya@gmail.com>
2024-09-19 13:26:09 +0000

    Initial Commits

---
+++ src/components/MoviesSlider/index.js
@@ -0,0 +1,26 @@
+import Slider from 'react-slick'
+import MovieItem from '../MovieItem'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+const MoviesSlider = props => {
+  const settings = {
+    dots: false,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+  }
+  const {moviesList} = props
+
+  return (
+    <>
+      <Slider {...settings}>
+        {moviesList.map(eachMovie => (
+          <MovieItem key={eachMovie.id} movieDetails={eachMovie} />
+        ))}
+      </Slider>
+    </>
+  )
+}
+
+export default MoviesSlider
